4cb0fb38f680f28e91bc1c1688048dd52c6dcde5
Patrick Martins <pattrickpx99@gmail.com>
2020-05-03 20:21:17 -0300

    correção de erro e refatoração
    * correção de erro apresentado durante o teste com o Insomnia
    * adição de alterações sugeridas

--- server.py
+++ server.py
@@ -3,60 +3,49 @@ import sys
 import threading
 import os 
 import time
+import html_parser
+# ngrok http 45654
+def head(path):
+    header='HTTP/1.1 200 OK\r\n'
+    header+='Connection: close\r\n'
+    # header+='Content-Encoding: UTF-8\r\n'
+    header+='Content-Length: ' + str(os.path.getsize(path)) + '\r\n'
+    header+='Expires: -1\r\n'
+    header+='Last-Modified: ' + str(time.ctime(os.path.getmtime(path))) + '\r\n'
+    header+='Content-Type: text/html; charset=UTF-8\r\n'
+    header+='\r\n'
+    return header
+
 def handleClient(conn,addr):
     print(f'criando thred para client {addr}')
     while True:
         
         data = conn.recv(1024)
         msg= str(data,'utf8')
-        # if '#close' in msg:
-        #     conn.close()
-        #    
-        print(f'[{addr}]: {msg}')
-        msg=msg.split(" ")
 
-        def head(msg):
-            conn.sendall(b'HTTP/1.1 200 OK\n')
-            
+        print(f'[{addr}]: {msg}')
+        awn=html_parser.process_message('svr',msg)
+        print(awn)
+        if os.path.exists(awn['path']): #verifica de o arquivo existe
+
+            if awn['command'] =='GET' :
+                dado=open(awn['path'])
+                dado=head(awn['path'])+('\n'.join(dado.readlines()))
+                conn.sendall(dado.encode())
+            # head="HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: "+str(len(enviar))+"\r\n\r\n"
+            elif awn['command']=='HEAD':
+                conn.sendall(head(awn['path']).encode())
+            elif awn['command'] == 'POST':
+                print('POST')             
+                
+        else:
+            conn.sendall(b'HTTP/1.1 404 ERRO\n')
             conn.sendall(b'Connection: close\n')
-            conn.sendall(b'Content-Encoding: UTF-8\n')
-            conn.sendall(('Content-Length: ' + str(os.path.getsize(msg)) + '\n').encode())
-            conn.sendall(b'Expires: -1\n')
-            conn.sendall(('Last-Modified: ' + str(time.ctime(os.path.getmtime(msg)))+ '\n').encode())
             conn.sendall(b'Content-Type: text/html\n')
             conn.sendall(b'\n')
-
-        if 'GET' in msg:
-            print('#############################')
-            print()
-            print(f'pegando informação {msg[1][1:]}')
-           
-
+            conn.sendall(b'<html> <header> <title>ERRO 404</title>  </header>  <body > <H1>ERROR 404 PAGE NOT FOUND </H1> </body></html>')
         
-            # head="HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: "+str(len(enviar))+"\r\n\r\n"
-            arquivos = os.listdir() 
-            print(arquivos)
-            
-                
-            if msg[1][1:] in arquivos:
-                
-                head(msg[1][1:])
-                dado=open(msg[1][1:])
-                dado='\n'.join(dado.readlines())
-                conn.sendall(dado.encode())
-
-                
-            else:
-                conn.sendall(b'HTTP/1.1 404 ERRO\n')
-                conn.sendall(b'Connection: close\n')
-                conn.sendall(b'Content-Type: text/html\n')
-                conn.sendall(b'\n')
-                conn.sendall(b'\n')
-                conn.sendall(b'<html> <header> <title>ERRO 404</title>  </header>  <body > <H1>ERROR 404 PAGE NOT FOUND </H1> </body></html>')
-        
-        if 'HEAD' in msg:
-            head(msg[1][1:])
-
+    
         break
     conn.close()    
         # conn.sendall(data)
